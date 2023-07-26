9dbb0bdbecdda867d3900aa7892a70fa281f97d9
Vikryan <122822310+Vikryan@users.noreply.github.com>
2023-07-26 03:54:14 +0300

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -3,7 +3,7 @@
  * handle_print - A function that prints an argument based on its type
  * @fmt: Formatted string in which to print the arguments.
  * @list: List of arguments to be printed.
- * @ind: index.
+ * @ind: ind.
  * @buffer: Buffer array to handle print.
  * @flags: Computes active flags
  * @width: get width.
